83cdf013a20b394cf896fe04bd9af3808f9d4e29
Tu Nombre <er610817@gmail.com>
2025-04-11 17:58:13 -0600

    Actualizacion de Contactos

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -14,28 +14,36 @@ const ContactPage = () => {
       </p>
       <div className="contact-icons">
         <div className="icon-item">
-          <div className="icon-circle">
-            <i className="fab fa-whatsapp"></i>
-          </div>
-          <p>Whatsapp<br />ejemplo numérico</p>
+          <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
+            <div className="icon-circle">
+              <i className="fab fa-whatsapp"></i>
+            </div>
+          </a>
+          <p>Whatsapp<br />+1 (234) 567-890</p>
         </div>
         <div className="icon-item">
-          <div className="icon-circle">
-            <i className="fab fa-facebook"></i>
-          </div>
-          <p>Facebook<br />ejemplo</p>
+          <a href="https://facebook.com/ejemploempresa" target="_blank" rel="noopener noreferrer">
+            <div className="icon-circle">
+              <i className="fab fa-facebook"></i>
+            </div>
+          </a>
+          <p>Facebook<br />@ejemploempresa</p>
         </div>
         <div className="icon-item">
-          <div className="icon-circle">
-            <i className="fab fa-twitter"></i>
-          </div>
-          <p>Twitter<br />ejemplo</p>
+          <a href="https://twitter.com/ejemploempresa" target="_blank" rel="noopener noreferrer">
+            <div className="icon-circle">
+              <i className="fab fa-twitter"></i>
+            </div>
+          </a>
+          <p>Twitter<br />@ejemploempresa</p>
         </div>
         <div className="icon-item">
-          <div className="icon-circle">
-            <i className="fas fa-globe"></i>
-          </div>
-          <p>Etc.<br />ejemplo</p>
+          <a href="https://www.ejemploempresa.com" target="_blank" rel="noopener noreferrer">
+            <div className="icon-circle">
+              <i className="fas fa-globe"></i>
+            </div>
+          </a>
+          <p>Sitio Web<br />www.ejemploempresa.com</p>
         </div>
       </div>
     </div>
